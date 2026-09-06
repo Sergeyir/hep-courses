1a1ba86f41d0884aa05ef46eef7d43b9210ba044
sergeyir <antsupov0124@gmail.com>
2026-09-06 16:05:20 +0300

    adding new file

---
+++ jets3/include/RunPythia.hpp
@@ -0,0 +1,28 @@
+/** 
+ *  @file   RunPythia.hpp
+ *  @brief  Contains simplest case that shows how the data can be generated with PYTHIA8 and LHAPDF6 and processed with FASTJET3 for jets selection 
+ *
+ *  This file is a part of a project hep-courses/jets3 (https://github.com/Sergeyir/hep-courses/jets3).
+ *
+ *  @author Sergei Antsupov (antsupov0124@gmail.com)
+ **/
+#ifndef RUN_PYTHIA_HPP
+#define RUN_PYTHIA_HPP
+
+#include <iostream>
+#include <vector>
+#include <filesystem>
+
+#include "LHAPDF/LHAPDF.h"
+
+#include "Pythia8/Pythia.h"
+
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/ClusterSequence.hh"
+
+#include "yaml-cpp/yaml.h"
+
+#include "TFile.h"
+#include "TH1D.h"
+
+#endif /* RUN_PYTHIA_HPP */
